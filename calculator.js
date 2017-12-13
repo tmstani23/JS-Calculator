a72e43db6c65eccceaff7f01db7da8e097924594
Tim Work Laptop <tmstani23@gmail.com>
2017-12-13 16:05:53 -0600

    Calc Update 2
    Added function to store numbers.  Added functionality to store numbers in an array then parse them into an int when called.  Added conditionals to check if operation buttons have been used and for addition.  Added function to display parsed numbers to the screen

--- calculator.js
+++ calculator.js
@@ -1,8 +1,26 @@
-let buttonInput1 = [];
-let buttonInput2 = [];
+let numInput1 = [];
+let numInput2 = [];
+let numDisplay1 = "";
+let numDisplay2 = "";
+let add = false;
+let hasOperation = false
 
-function addNum(num1, num2) {
-    return num1 + num2;
+function storeNum(num) {
+    //if operation button clicked:
+    if (hasOperation == true){
+        numDisplay2 += "" + num;
+        numInput2.push(num);
+        outputNum(numDisplay2);    
+    }
+    else {
+        numDisplay1 += "" + num;
+        numInput1.push(num);
+        outputNum(numDisplay1);
+    }
+}
+function addNum() {
+    add = true;
+    hasOperation = true;
 }
 function subtractNum(num1, num2) {
     return num1 - num2;
@@ -14,11 +32,28 @@ function multNum(num1, num2) {
     return num1 * num2;
 }
 function outputNum(num) {
-    buttonInput1.push(num)
+    
     document.getElementById("outputP").innerHTML = num;
 }
 function outputAns() {
-    document.getElementById("outputP").innerHTML = buttonInput1;
+    //merge all numbers in array into one number:
+    numInput1 = parseInt(numInput1.join(''));
+    numInput2 = parseInt(numInput2.join(''));
+    
+    if (add == true) {
+        console.log(typeof(numInput1));
+        document.getElementById("outputP").innerHTML = numInput1 + numInput2;    
+    }
+    //reset all flags:
+    hasOperation = false;
+    add = false;
+    //reset second number:
+    numInput2 = [];
+    numInput1 = tempNum;
+    
+
+    
+    
 
 }
 
