72d185aa676595785ec81ee2d664fb7659f36178
Tim Work Laptop <tmstani23@gmail.com>
2017-12-14 16:08:25 -0600

    Calc Update 3
    Added remaining operation functions.  Added clear all button functionality and conditional logic.

--- calculator.js
+++ calculator.js
@@ -2,8 +2,13 @@ let numInput1 = [];
 let numInput2 = [];
 let numDisplay1 = "";
 let numDisplay2 = "";
+let tempAns = 0;
 let add = false;
+let subt = false;
+let mult = false;
+let divide = false;
 let hasOperation = false
+let clearClicked = false;
 
 function storeNum(num) {
     //if operation button clicked:
@@ -22,14 +27,39 @@ function addNum() {
     add = true;
     hasOperation = true;
 }
-function subtractNum(num1, num2) {
-    return num1 - num2;
+function subtNum() {
+    subt = true;
+    hasOperation = true;
 }
-function divideNum(num1, num2) {
-    return num1 / num2;
+function divideNum() {
+    divide = true;
+    hasOperation = true;
 }
-function multNum(num1, num2) {
-    return num1 * num2;
+function multNum() {
+    mult = true;
+    hasOperation = true;
+}
+function clearAll(clear) {
+    
+    numInput2 = [];
+    numInput1 = [];
+    numDisplay1 = "";
+    numDisplay2 = "";
+    hasOperation = false;
+    add = false;
+    subt = false;
+    mult = false;
+    divide = false;
+
+    if(clear == true){
+        numInput1 = [0];
+        outputNum(0);
+    }
+    
+    
+   
+  
+       
 }
 function outputNum(num) {
     
@@ -41,15 +71,31 @@ function outputAns() {
     numInput2 = parseInt(numInput2.join(''));
     
     if (add == true) {
-        console.log(typeof(numInput1));
-        document.getElementById("outputP").innerHTML = numInput1 + numInput2;    
+        //console.log(typeof(numInput1));
+        tempAns = (numInput1 + numInput2);
+        document.getElementById("outputP").innerHTML = tempAns;
     }
-    //reset all flags:
-    hasOperation = false;
-    add = false;
-    //reset second number:
-    numInput2 = [];
-    numInput1 = tempNum;
+    if (subt == true) {
+        //console.log(typeof(numInput1));
+        tempAns = (numInput1 - numInput2);
+        document.getElementById("outputP").innerHTML = tempAns;
+    }
+    if (mult == true) {
+        //console.log(typeof(numInput1));
+        tempAns = (numInput1 * numInput2);
+        document.getElementById("outputP").innerHTML = tempAns;
+    }
+    if (divide == true) {
+        //console.log(typeof(numInput1));
+        tempAns = (numInput1 / numInput2);
+        document.getElementById("outputP").innerHTML = tempAns;
+    }
+    //call clear function to reset variables
+    clearAll(false);
+    //push answer back into numInput1 array
+    numInput1.push(tempAns);
+    console.log(numInput1); 
+
     
 
     
