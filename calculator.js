2ed02979aee4829507a631f5074ed2296fcc3fd3
Tim Work Laptop <tmstani23@gmail.com>
2017-12-15 15:40:45 -0600

    Calc Update 4
    Added functionality for chaining multiple operations.  Refactored some of the conditional code.  Added operations to the display.

--- calculator.js
+++ calculator.js
@@ -1,100 +1,144 @@
-let numInput1 = [];
-let numInput2 = [];
-let numDisplay1 = "";
-let numDisplay2 = "";
+let numPrev = [];
+let numCurrent = [];
+let numDisplay1 = [];
+let numDisplay2 = [];
+let numDisplay = [];
 let tempAns = 0;
 let add = false;
 let subt = false;
 let mult = false;
 let divide = false;
-let hasOperation = false
+let isCurrent = false
 let clearClicked = false;
 
 function storeNum(num) {
     //if operation button clicked:
-    if (hasOperation == true){
-        numDisplay2 += "" + num;
-        numInput2.push(num);
-        outputNum(numDisplay2);    
+    
+    if (isCurrent == true && numPrev.length > 0){
+        //numDisplay2.push(num);
+        numDisplay.push(num);
+        numCurrent.push(num);
+        //outputNum(numDisplay2);
+        isCurrent = false;
+        outputAns();
+        
     }
+    else if (isCurrent == true){
+        //numDisplay2.push(num);
+        numDisplay.push(num);
+        numCurrent.push(num);
+        //outputNum(numDisplay2);
+        outputNum(numDisplay);
+        isCurrent = false;
+        
+            
+    }
+
     else {
-        numDisplay1 += "" + num;
-        numInput1.push(num);
-        outputNum(numDisplay1);
+        //numDisplay1.push(num);
+        numDisplay.push(num)
+        numPrev.push(num);
+        //outputNum(numDisplay1);
+        outputNum(numDisplay);
     }
 }
 function addNum() {
+   
     add = true;
-    hasOperation = true;
+    isCurrent = true;
+    numDisplay.push('+');
+    numDisplay.join(" ");
+    outputNum(numDisplay);
+    
+    
 }
 function subtNum() {
     subt = true;
-    hasOperation = true;
+    isCurrent = true;
+    numDisplay.push(" -");
+    outputNum(numDisplay);
 }
 function divideNum() {
     divide = true;
-    hasOperation = true;
+    isCurrent = true;
+    numDisplay.push(" /");
+    outputNum(numDisplay);
 }
 function multNum() {
     mult = true;
-    hasOperation = true;
+    isCurrent = true;
+    numDisplay.push("*");
+    outputNum(numDisplay);
 }
 function clearAll(clear) {
     
-    numInput2 = [];
-    numInput1 = [];
-    numDisplay1 = "";
-    numDisplay2 = "";
-    hasOperation = false;
-    add = false;
-    subt = false;
-    mult = false;
-    divide = false;
+    numCurrent = [];
+    numPrev = [];
+    //numDisplay1 = [];
+    //numDisplay2 = [];
+    numDisplay = [];
+    //isCurrent = false;
+    //add = false;
+    //subt = false;
+    //mult = false;
+    //divide = false;
+    //isCurrent = false;
 
     if(clear == true){
-        numInput1 = [0];
+        numPrev = [0];
         outputNum(0);
     }
-    
-    
-   
-  
-       
 }
+
+// function clearCurrent() {
+//     if (isCurrent == true) {
+//         numCurrent = [0];
+//     }
+//     else {
+//         numPrev = [0];
+//     }
+// }
 function outputNum(num) {
     
+    console.log(num);
     document.getElementById("outputP").innerHTML = num;
 }
 function outputAns() {
     //merge all numbers in array into one number:
-    numInput1 = parseInt(numInput1.join(''));
-    numInput2 = parseInt(numInput2.join(''));
+    numPrev = parseInt(numPrev.join(''));
+    numCurrent = parseInt(numCurrent.join(''));
     
     if (add == true) {
-        //console.log(typeof(numInput1));
-        tempAns = (numInput1 + numInput2);
+        //console.log(typeof(numPrev));
+        tempAns = (numPrev + numCurrent);
         document.getElementById("outputP").innerHTML = tempAns;
+        add = false;
     }
     if (subt == true) {
-        //console.log(typeof(numInput1));
-        tempAns = (numInput1 - numInput2);
+        //console.log(typeof(numPrev));
+        tempAns = (numPrev - numCurrent);
         document.getElementById("outputP").innerHTML = tempAns;
+        subt = false;
     }
     if (mult == true) {
-        //console.log(typeof(numInput1));
-        tempAns = (numInput1 * numInput2);
+        //console.log(typeof(numPrev));
+        tempAns = (numPrev * numCurrent);
         document.getElementById("outputP").innerHTML = tempAns;
+        mult = false;
     }
     if (divide == true) {
-        //console.log(typeof(numInput1));
-        tempAns = (numInput1 / numInput2);
+        //console.log(typeof(numPrev));
+        tempAns = (numPrev / numCurrent);
         document.getElementById("outputP").innerHTML = tempAns;
+        divide = false;
     }
     //call clear function to reset variables
     clearAll(false);
-    //push answer back into numInput1 array
-    numInput1.push(tempAns);
-    console.log(numInput1); 
+    //push answer back into numPrev array
+    numPrev.push(tempAns);
+    numDisplay.push(tempAns);
+    //console.log(numPrev); 
+    //console.log(numCurrent);
 
     
 
