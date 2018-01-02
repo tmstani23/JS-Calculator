b352b328cc326bfe65f17f2f7bbd6627a188b855
Tim Work Laptop <tmstani23@gmail.com>
2018-01-02 15:27:44 -0600

    Update 10
    Trimmed superfluous code.  Added commentary.  Working as intended.

--- calculator.js
+++ calculator.js
@@ -9,72 +9,62 @@ let divide;
 let isCurrent = false
 let clear = false;
 
+//Function that stores numbers when a number button is clicked
 function storeNum(num) {
-    //if operation button clicked:
-    
-    // if (isCurrent == true && numPrev.length > 0){
-    //     //numDisplay2.push(num);
-    //     numDisplay.push(num);
-    //     numCurrent.push(num);
-    //     //outputNum(numDisplay2);
-    //     isCurrent = false;
-    //     outputAns();
-        
-    // }
-    
+    //If one number is stored and an operation button has been clicked:
     if (numPrev.length > 0 && isCurrent == true){
-        //console.log("true");
-        //Need to figure out how to chain subtract.  Can't distinguish between add and subtract here
-        //add = true;
+        //Store the number in a number display array:
         numDisplay.push(num);
+        //Store the function input number in the numCurrent array:
         numCurrent.push(num);
         //console.log(numPrev + "numPrev", numCurrent + "numCurrent");
+        //Call the output number function to display 
+            //and pass in the numDisplay array:
         outputNum(numDisplay);
-        
-        
-            
     }
-
     else {
-        //numDisplay1.push(num);
+        //Add input number to previous and display arrays:
         numDisplay.push(num)
         numPrev.push(num);
-        //outputNum(numDisplay1);
+        //Call the output number function to display 
+            //and pass in the numDisplay array:
         outputNum(numDisplay);
     }
 }
+//These functions assign an operation tag, resets all other operation tags, 
+    //and calls the output answer function:
 function addNum() {
-        if (numPrev.length >= 1 && numCurrent.length >=1) {
+    //If numbers are stored in the prev and current arrays:    
+    if (numPrev.length >= 1 && numCurrent.length >=1) {
             //console.log(true + "addnum")
+            //Call the answer function then apply operation tags:
             outputAns();
             add = true;
             mult = false;
             divide = false;
             subt = false;
             isCurrent = true;
+            //Add operation symbol to the display array:
             numDisplay.push(` + `);
-            //numDisplay.join(" ");
             outputNum(numDisplay);
         
-        }
-        else{
-        //console.log(tempAns + "after plus");
-        add = true;
-        mult = false;
-        divide = false;
-        subt = false;
-        isCurrent = true;
-        numDisplay.push(` + `);
-        //console.log(false + "addnum")
-        //console.log(numDisplay);
-        outputNum(numDisplay);
-        outputAns();
-        }
+    }
+    else{
+    //console.log(tempAns + "after plus");
+    //Apply operation tags first then call answer function:
+    add = true;
+    mult = false;
+    divide = false;
+    subt = false;
+    isCurrent = true;
+    //Add operation symbol to the display array:
+    numDisplay.push(` + `);
+    outputNum(numDisplay);
+    outputAns();
+    }
          
-    
-    
-    
 }
+
 function subtNum() {
     if (numPrev.length >= 1 && numCurrent.length >=1) {
         outputAns()
@@ -98,6 +88,7 @@ function subtNum() {
         outputAns()
     }
 }
+
 function divideNum() {
     if (numPrev.length >= 1 && numCurrent.length >=1) {
         outputAns()
@@ -121,6 +112,7 @@ function divideNum() {
         outputAns()
     }
 }
+
 function multNum() {
     if (numPrev.length >= 1 && numCurrent.length >=1) {
         outputAns()
@@ -144,29 +136,30 @@ function multNum() {
         outputAns()
     }
 }
+//This function clears all arrays and display values
+//Used when the AC or "=" button is clicked:
 function clearAll(clear) {
-    
+    //if equal sign button was clicked previously:
     if(clear == true){
         numCurrent = [];
         numPrev = [];
+        //Set display array to contain temporary answer:
         numDisplay = [tempAns];
-        //tempAns = 0;
         add = false;
         subt = false;
         mult = false;
         divide = false;
         isCurrent = false;
         clear = false;
-        
         outputNum(numDisplay);
-        
-       // outputAns();
     }
+    //If AC button was clicked previously:
     else if(clear == "ac") {
-        
         numCurrent = [];
         numPrev = [];
+        //Set display array to contain temporary answer:
         numDisplay = [0];
+        //Set temp answer to 0;
         tempAns = 0;
         add = false;
         subt = false;
@@ -175,61 +168,43 @@ function clearAll(clear) {
         isCurrent = false;
         clear = false; 
         document.getElementById("outputP").innerHTML = tempAns;
+        //Display 0
         outputNum(0);
-        outputAns();
-
     }
+    //Else only reset num current and prev arrays:
     else {
         numCurrent = [];
         numPrev = [];
     }
-   
 
-   
 }
-
+//This function adjusts arrays and display when the CE button is clicked:
 function clearCurrent() {
+    //Reset the current array to an array containing 0:
     numCurrent = [0];
-    if (numPrev.length == 1 && numCurrent.length == 0) {
-        console.log("reset answer to zero");
-        numPrev = [0];
-        
-    }
-    //console.log(numCurrent.length + "current");
-    
-    //if 
-    if (numDisplay.length >= 1) {
-        //numDisplay = numDisplay.pop();
-        // numDisplay.pop();
-        // numDisplay = [numDisplay];
-        numDisplay = [tempAns];
-        //numDisplay.push(numCurrent);
-        
-        console.log(numDisplay + "pop");
-    }
-    else {
-        numDisplay = numCurrent;
-        console.log("numdisplay = numcurrent");
-    }
-    //numDisplay = [numPrev, numCurrent];
-    
+    //Reset the diplay to temp answer
+    numDisplay = [tempAns];
+    //Output the display: 
     outputNum(numDisplay);
 }
+//This function outputs an input array or number on the page by adjusting the html
 function outputNum(num) {
+    //If the first display element is a 0
+        //and the total amount of numbers in the display is > 2
     if(numDisplay[0] == 0 && numDisplay.length >=2){
+        //Remove the 0 and reset the display array:
         numDisplay.shift();
         numDisplay = [numDisplay];
     }
-    
+    //Convert the display array into a string with no spaces:
     num = numDisplay.join('');
-    
-    //numDisplay = [numDisplay];
-    console.log(num);
+    //console.log(num);
+    //Display the string to the screen:
     document.getElementById("numDisplayP").innerHTML = num;
 }
+//This function performs main calculator operations and outputs the answer to the screen:
 function outputAns(clear) {
-    //add error message if number is too long(numprev,numcurr, or tempans)
-    //merge all numbers in array into one number:
+    //If either of the number arrays are empty add a zero to the array:
     if (numCurrent.length == 0) {
         //console.log("numCurrentlength is 0")
         numCurrent.push(0);
@@ -238,43 +213,35 @@ function outputAns(clear) {
         //console.log("numCurrentlength is 0")
         numPrev.push(0);
     }
-    
+    //Join the number arrays into strings then parse them - 
+        //into decimal numbers:
     numPrev = parseFloat(numPrev.join(''));
     numCurrent = parseFloat(numCurrent.join(''));
-    console.log(numPrev + "numPrev");
-    console.log(numCurrent + "numCurrent");
+    //console.log(numPrev + "numPrev");
+    //console.log(numCurrent + "numCurrent");
     
-    
+    //These conditions check if an operation is true -
+        //then perform the operation and display to screen:
     if (add == true) {
-        //console.log("add is true");
-        // if (tempAns != 0 && numCurrent == 0 && numPrev == 0)  {
-        //   console.log(tempAns + "tempanstrue");  
-        //   tempAns = tempAns;  
-        // }
-        // else {
-            //console.log(numPrev, numCurrent);
-            tempAns = (numPrev + numCurrent);
-            //console.log(tempAns + "tempans");
-        // }
-        
+       tempAns = (numPrev + numCurrent);
+        //console.log(tempAns + "tempans");
         document.getElementById("outputP").innerHTML = tempAns;
-        
-        
     }
+    
     if (subt == true) {
         //console.log("subt is true");
         tempAns = (numPrev - numCurrent);
         document.getElementById("outputP").innerHTML = tempAns;
-        //subt = false;
     }
+
     if (mult == true) {
         //console.log("mult is true");
-        //problem is here:
-        
+        //If only one number has been entered multiply it by one:
         if(numCurrent == 0) {
             tempAns = numPrev * 1;
             //console.log("is zero");
         }
+
         else {
             tempAns = numPrev * numCurrent;
             document.getElementById("outputP").innerHTML = tempAns;
@@ -282,7 +249,9 @@ function outputAns(clear) {
         }
         
     }
+    
     if (divide == true) {
+        //If only one number has been entered multiply it by one:
         if(numCurrent == 0) {
             tempAns = numPrev * 1;
             //console.log("is zero");
@@ -290,18 +259,15 @@ function outputAns(clear) {
         else {
             tempAns = numPrev / numCurrent;
             document.getElementById("outputP").innerHTML = tempAns;
-
         }
         
     }
-    //call clear function to reset variables
-    //clear = false;
+    //Call clear function to reset variables
     clearAll(clear);
-    //push answer back into numPrev array
+    //Push answer back into numPrev array
     numPrev.push(tempAns);
-    //numDisplay.push(tempAns);
+    
     //console.log(numPrev + "numPrev"); 
     //console.log(numCurrent + "numCurrent");
-
 }
 
